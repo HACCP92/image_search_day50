2d3b58cf8b8d02304d2155f8d79df59b29be729d
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-12-23 17:25:21 +0900

    [이미지 검색앱] detail_screen router 연결

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_search_day50/data/repository/pixabay_repository.dart';
+import 'package:image_search_day50/domain/model/photo.dart';
 import 'package:image_search_day50/domain/use_case/get_top_five_most_view_use_case.dart';
+import 'package:image_search_day50/presentation/detail/detail_screen.dart';
 import 'package:image_search_day50/presentation/main/main_screen.dart';
 import 'package:image_search_day50/presentation/main/main_view_model.dart';
 import 'package:provider/provider.dart';
@@ -20,6 +22,15 @@ final GoRouter router = GoRouter(
           child: const MainScreen(),
         );
       },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            final photo = state.extra as Photo;
+            return DetailScreen(photo: photo);
+          },
+        ),
+      ],
     ),
   ],
 );
